2a616c9602133dec0f3ab8e8fe5b0705631f1aee
Sasit Srirat <55717534+sasitsrirat@users.noreply.github.com>
2022-10-11 00:49:11 +0700

    Add files via upload

---
+++ Java File/src/Menus.java
@@ -0,0 +1,16 @@
+import java.applet.*;
+import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Menus 
+{
+	public String name; // +
+	public int price; // +
+	public int totalDishes; // +
+
+}
+
